b47698def6dff7531bdf761436ac40f9784be3d0
Mayankkkyadav <11997mayank@gmail.com>
2020-09-13 16:14:01 +0530

    Another One

---
+++ MavenPro/src/test/java/com/lti/test/TestArithmetic.java
@@ -0,0 +1,28 @@
+package com.lti.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+
+import com.lti.code.Arithmetic;
+
+public class TestArithmetic {
+	private Arithmetic art;
+	
+	@Before
+	public void init(){
+		art=new Arithmetic();
+			
+	}
+	@Test
+	public void TestSum() {
+		assertEquals(10,art.sum(4, 6));
+		
+	}
+	@Test
+	public void Testmax() {
+		assertEquals(30,art.max(30, 20));
+	}
+	
+
+}
